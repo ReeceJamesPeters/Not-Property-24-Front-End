409b179018119a49c611167f37667477790a21c6
TristanLBBD <tristanl@bbd.co.za>
2022-05-19 14:04:19 +0200

    Listings page added and change of some components

--- src/fetchProperties.js
+++ src/fetchProperties.js
@@ -1,9 +1,9 @@
 //import {allPropertiesEndPoint} from './utils';
 
 const fetchProperties = async () => {
-    const response = await fetch('https://tilnwqzb47.execute-api.af-south-1.amazonaws.com/dev/api/property').catch((err) => console.log(err));
-    if (response){
-        return response.json();
+    const response = await fetch('https://tilnwqzb47.execute-api.af-south-1.amazonaws.com/dev/api/listingType').catch((err) => console.log(err));
+    if (response.ok){
+        return response.json();   
     }
     return response;
 };
