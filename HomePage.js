9bfa04dca2ebab2441e3fad3c7a132770d12bccc
Peters <reece.peters@vcontractor.co.za>
2022-05-19 19:14:40 +0200

    more stuff

--- HomePage.js
+++ HomePage.js
@@ -1,15 +1,14 @@
 import { fetchProperties } from "./src/fetchProperties.js";
 import { setStorageItem, getElement } from "./src/utils.js";
-import { displayProperties } from "./src/displayProperties.js";
+import { displayProperties, displayPropertiesFiltered } from "./src/displayProperties.js";
 import { setupprops, props } from "./src/properties.js";
-import { displayPropertiesFilters } from "./Listings.js";
 
 const init = async () => {
     const properties = await fetchProperties();
     if (properties){
         await setupprops(properties);
         console.log(properties);
-        displayProperties(props, getElement('.Properties'))
+        displayProperties(props, getElement('.props'))
     }
 };
 
@@ -22,20 +21,39 @@ document.querySelector(".SubmitSearch").addEventListener("click", () => {
     const city = document.getElementById('city');
     let filteredProps = props;
     if (bedrooms.value){
-    filteredProps = filteredProps.filter((p) => {
-        if (p.NoOfBedrooms >= bedrooms.value)
-            return p;
-    });
-}
+        filteredProps = filteredProps.filter((p) => {
+            if (p.NoOfBedrooms >= bedrooms.value)
+                return p;
+        });
+    }
     if (bathrooms.value){
-    filteredProps = filteredProps.filter((p) => {
-        if (p.NoOfBathrooms >= bathrooms.value)
-            return p;
-    });
-}
+        filteredProps = filteredProps.filter((p) => {
+            if (p.NoOfBathrooms >= bathrooms.value)
+                return p;
+        });
+    }
+    if (maxprice.value){
+        filteredProps = filteredProps.filter((p) => {
+            if (p.Price < maxprice.value)
+                return p;
+        });
+    }
+    if (minprice.value){
+        filteredProps = filteredProps.filter((p) => {
+            if (p.Price > minprice.value)
+                return p;
+        });
+    }
+    if (city.value){
+        filteredProps = filteredProps.filter((p) => {
+            if (p.City < city.value)
+                return p;
+        });
+    }
+    
     
     console.log(filteredProps);
-    displayPropertiesFilters(filteredProps, getElement('.Properties'));
+    displayPropertiesFiltered(filteredProps, getElement('.props'));
 });
 
 window.addEventListener('DOMContentLoaded', init);
--- src/displayProperties.js
+++ src/displayProperties.js
@@ -1,6 +1,6 @@
 
 const displayProperties = (properties, element) => {
-    let x = properties.map((p) => {
+    let propCards = properties.map((p) => {
         const {PropertyID,Price,PropertyName,Street,NoOfBedrooms,NoOfBathrooms,ImageURL} = p;
            return `<div class="Propertycard">
                 <a href="PropertyDetailsPage.html?id=${PropertyID}">
@@ -17,14 +17,55 @@ const displayProperties = (properties, element) => {
                 </div>
         </div>` 
     })
-    let htmlThing = "";
-    for (var c=0; c<3; c++){
-        htmlThing += x[c];
+    let htmlThing = `<div class="Properties">`;
+    let offset = i * 3;
+    for (let c = 0; c < 3; c++) {
+      htmlThing += propCards[c + offset];
     }
+    htmlThing += "</div>";
 
     element.innerHTML = htmlThing;
     
 };
 
-export {displayProperties};
+const displayPropertiesFiltered = (properties, element) => {
+    let propCards = properties.map((p) => {
+      const {
+        PropertyID,
+        Price,
+        PropertyName,
+        Street,
+        NoOfBedrooms,
+        NoOfBathrooms,
+        ImageURL,
+      } = p;
+      return `<div class="Propertycard">
+                  <a href="PropertyDetailsPage.html?id=${PropertyID}">
+                      <img src="${ImageURL}" alt="House-Image" style="width:100%">
+                  </a>
+                  <div class="PropertyInfo">
+                      <h3><b>R ${Price}</b></h3>
+                      <p><b>${PropertyName}</b></p>
+                      <p>${Street}</p>
+                      <row class="icons">
+                          <i class="fa fa-home" aria-hidden="true">${NoOfBedrooms}</i>
+                          <i class="fa fa-bath" aria-hidden="true">${NoOfBathrooms}</i>
+                      </row>
+                  </div>
+          </div>`;
+    });
+    let divCount = Math.floor(propCards.length / 3);
+    let htmlThing = "";
+    for (let i = 0; i < divCount; i++) {
+      htmlThing += `<div class="Properties">`;
+      let offset = i * 3;
+      for (let c = 0; c < 3; c++) {
+        htmlThing += propCards[c + offset];
+      }
+      htmlThing += "</div>";
+    }
+    element.innerHTML = htmlThing;
+  };
+
+export {displayProperties, displayPropertiesFiltered};
 
